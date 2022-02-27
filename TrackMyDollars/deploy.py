d6c7ccbb550fa9a45eca4da78a5623c138761d03
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2022-02-27 09:38:46 -0600

    Add SSL settings to try to force HTTPS

--- TrackMyDollars/deploy.py
+++ TrackMyDollars/deploy.py
@@ -5,3 +5,7 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "TrackMyDollars.deploy_settings")
 
 application = get_wsgi_application()
+
+# Added to force SSL based on https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
