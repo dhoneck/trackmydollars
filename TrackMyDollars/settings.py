324d9e6b34027fa3edc8c22790a6c5b9351c618f
Dustin Honeck <13143062+dhoneck@users.noreply.github.com>
2022-02-27 09:42:09 -0600

    Try to force SSL again since last commit did not work

--- TrackMyDollars/settings.py
+++ TrackMyDollars/settings.py
@@ -66,6 +66,10 @@ MIDDLEWARE = [
     'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
+# Added to force SSL based on https://help.heroku.com/J2R1S4T8/can-heroku-force-an-application-to-use-ssl-tls
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+SECURE_SSL_REDIRECT = True
+
 ROOT_URLCONF = 'TrackMyDollars.urls'
 
 TEMPLATES = [
